97542ca7621ab90f4105da8546c36b33ea6b8387
Ryo Enkita <mildgo7925@gmail.com>
2022-03-07 16:24:53 +0900

    add main loop

--- main.rb
+++ main.rb
@@ -1,1 +1,11 @@
-ruby/crystal/diamond
+require 'mittsu'
+
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
+ASPECT = SCREEN_WIDTH.to_f / SCREEN_HEIGHT.to_f
+
+renderer = Mittsu::OpenGLRenderer.new width: SCREEN_WIDTH, height: SCREEN_HEIGHT, title: 'Hello, World!'
+
+scene = Mittsu::Scene.new
+
+camera = Mittsu::PerspectiveCamera.new(75.0, ASPECT, 0.1, 1000.0)
